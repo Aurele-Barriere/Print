82460f4a8d748ceb88f1c974468e8d2a30b5f284
Aurèle <aurele.barriere@gmail.com>
2016-01-16 18:34:42 +0100

    my bash is rusty

--- print.sh
+++ print.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-echo "You want to print file " $1
-echo "Number of pages"
-pdfinfo $1 | grep 'Pages' | awk '{print $2}'
-
+echo "You want to print file " $1 
+nb=$(pdfinfo $1 | grep 'Pages' | awk '{print $2}')
+echo  $nb " pages"
 
