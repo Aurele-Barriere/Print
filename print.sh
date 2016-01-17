e5a224d9ab66faa357ed4fb8c52e77f79585adce
Aurèle <aurele.barriere@gmail.com>
2016-01-17 14:04:38 +0100

    added instructions

--- print.sh
+++ print.sh
@@ -9,14 +9,14 @@ then
 
 echo "Even"
 lp -o page-set=odd $1
-read -p "Do ... and press [Enter] to continue"
+read -p "Flip the printed pages and press [Enter] to continue"
 lp -o page-set=even -o outputorder=reverse $1
 
 else
 
 echo "Odd"
 lp -o page-set=odd $1
-read -p "Do ... and press [Enter] to continue"
+read -p "Flip the printed pages without the last one and press [Enter] to continue"
 lp -o page-set=even -o outputorder=reverse $1
 
 fi
