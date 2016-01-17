466c4fe1ec68f4dc3b1855b617b3dd0ce9cba9f5
Aurèle <aurele.barriere@gmail.com>
2016-01-17 14:42:55 +0100

    fixing the pause

--- print.sh
+++ print.sh
@@ -9,16 +9,18 @@ then
 
 echo "Even"
 lp -o page-set=odd $1
-read -p "Flip the printed pages and press [Enter] to continue"
+echo "Flip the printed pages and press [Enter] to continue"
+read _
 lp -o page-set=even -o outputorder=reverse $1
 
 else
 
 echo "Odd"
 lp -o page-set=odd $1
-read -p "Flip the printed pages without the last one and press [Enter] to continue"
+echo "Flip the printed pages without the last one and press [Enter] to continue"
+read _
 lp -o page-set=even -o outputorder=reverse $1
 
 fi
 
-
+echo "Done"
