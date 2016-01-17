10e6f25a16961c9e603e1b8b69c4c917be3c3827
Aurèle <aurele.barriere@gmail.com>
2016-01-17 15:02:33 +0100

    documenting

--- print.sh
+++ print.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+#This script is meant to help you printing recto-verso
+#Using CUPS and pdfinfo
+#Designed to work with Brother MFC-1910W
+
 echo "You want to print file " $1 
 nb=$(pdfinfo $1 | grep 'Pages' | awk '{print $2}')
 echo  $nb " pages."
